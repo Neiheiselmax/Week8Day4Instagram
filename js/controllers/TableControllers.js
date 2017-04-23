16f12d6d7f887c3d095c452ba506cbb1b3cc6695
Max Neiheisel <maxneiheisel@Max-Neiheisels-MacBook-2.local>
2017-04-23 18:27:29 -0400

    updated

--- js/controllers/TableControllers.js
+++ js/controllers/TableControllers.js
@@ -1,6 +1,15 @@
 (function() {
     'use strict';
 
+
+    $('#myModal').on('shown.bs.modal', function () {
+      $('#myInput').focus()
+    })
+
+
+
+
+
     angular
         .module('routing')
         .controller('TableController', function(API) {
@@ -10,7 +19,7 @@
             alert("goodJob")
             console.log(vm.item);
           if (isValid) {
-          let newItem = {"title": vm.item.title, "url": vm.item.url, "description": vm.item.description};
+          let newItem = {"title": vm.item.title, "url": vm.item.url, "description": vm.item.description, "likes":vm.item.likes};
           let post = API.postImage(newItem);
           post.then(res=>{
             console.log(res);
@@ -21,10 +30,18 @@
            }
         }
 
+        vm.changeQuantity = function(images,upOrNot){
+            (upOrNot)
+          {
+            images.likes++;
+          }
+          vm.image = response.data.images
+          }
+
         let image = API.getImage();
         image.then(function successCallback(response){
         console.log(response)
-        vm.image = response.data.images.url
+        vm.image = response.data.images
         })
 
         })
